21f6cf3c213850b3b281a85366c1575dbb53d5c4
Cory Robinson <coryrobinson42@gmail.com>
2018-05-17 07:34:46 -0700

    work

--- Build & Dev Tooling/flow & typescript/typescript/index.js
+++ Build & Dev Tooling/flow & typescript/typescript/index.js
@@ -2,15 +2,13 @@ function printName(person) {
   console.log(`
     Name: ${person.last}, ${person.first}
     Date of Birth: ${person.dob}
-  `);
+  `)
 }
 
 // *********************
 
-const cory: Person = {
-    first: "cory",
-    last: "robinson",
-    dob: new Date("01/06/1986")
-}
-
-printName(cory);
+printName({
+  first: 'cory',
+  last: 'robinson',
+  dob: new Date('01/06/1986'),
+});
